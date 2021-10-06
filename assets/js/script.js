ce821fe5b7dff3bda0821b343fcb40c4fa9e9857
Daniel Denton <danielryandenton@gmail.com>
2021-10-05 21:14:51 -0500

    create questions as objects

---
+++ assets/js/script.js
@@ -0,0 +1,65 @@
+var questionsArray = [
+    {
+        question: "Which of these is NOT a primitive data type?",
+        wrongA: "string",
+        wrongB: "boolean",
+        wrongC: "number",
+        correct: "function"
+    },
+    {
+        question: "Which of these expressions will evaluate to 'true'?",
+        wrongA: "1 === '1'",
+        wrongB: "'a' == 'A'",
+        wrongC: "'a' === 'A'",
+        correct: "1 == '1'",
+    },
+    {
+        question: "Which type of loop will execute at least once?",
+        wrongA: "for",
+        wrongB: "for...in",
+        wrongC: "while",
+        correct: "do...while",
+    },
+    {
+        question: "At which index is 'cat' given the following array?\n['dog', 'cat', 'mouse', 'bird']",
+        wrongA: "0",
+        wrongB: "2",
+        wrongC: "3",
+        correct: "1",
+    },
+    {
+        question: "When invoking a function, what goes inside the parentheses?",
+        wrongA: "parameter",
+        wrongB: "string",
+        wrongC: "variable",
+        correct: "argument",
+    },
+    {
+        question: "You can reference an object's properties by using bracket notation or _________ notation.",
+        wrongA: "variable",
+        wrongB: "mixed",
+        wrongC: "mathematical",
+        correct: "dot",
+    },
+    {
+        question: "What is the proper syntax for selecting the first element of an array?",
+        wrongA: "array.0",
+        wrongB: "array{0}",
+        wrongC: "array(0)",
+        correct: "array[0]",
+    },
+    {
+        question: "When a variable has not been assigned a value, its type is _________.",
+        wrongA: '""',
+        wrongB: "NaN",
+        wrongC: "symbol",
+        correct: "undefined",
+    },
+    {
+        question: "When a function is a property value on an object, it is called a _________.",
+        wrongA: "objFunction",
+        wrongB: "arm",
+        wrongC: "practice",
+        correct: "method",
+    }
+]
